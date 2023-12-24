3e85cd753f6b409c8808a613088a1110799e7f99
Denis Gudz <denis.tofam@gmail.com>
2023-12-24 11:04:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/header.jsx'
+import Meals from './components/meals.jsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Meals />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/meals.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react"
+import Meal from "./mealItem"
+
+function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([])
+  
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch('http://localhost:3000/meals')
+  
+      if (!response.ok) {
+        // ...
+      }
+  
+      const meals = await response.json()
+      setLoadedMeals(meals)
+      console.log(meals)
+    }
+
+    fetchMeals()
+  },[setLoadedMeals])
+  
+
+  return(
+    <ul id="meals">
+      {loadedMeals.map((meal) => {
+        return(
+          <Meal key={meal.id} meal={meal}/>
+        )
+      })}
+    </ul>
+  )
+
+}
+
+export default Meals
